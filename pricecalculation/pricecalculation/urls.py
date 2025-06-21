19ca2abdb6c5fa2b9e39da2d1c96057561dfc6d3
AAKASH KUMAR <aakashakay222@gmail.com>
2025-06-21 15:31:22 +0530

    Added all the Urls

--- pricecalculation/pricecalculation/urls.py
+++ pricecalculation/pricecalculation/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.urls import path
+from .views import CalculatePriceView
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('calculate-price/', CalculatePriceView.as_view(), name='calculate-price'),
 ]
